e744fdaa669490275d783828839701c44963bb85
rafael-cena <rafaelcena4@gmail.com>
2024-08-24 18:21:07 -0300

    funcionalidade mais de um placar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import Placar from "./Componentes/placar";
 function App() {
   return (
     <div className="App">
-      <Placar />
+      <Placar id="jp" nome="João" />
+      <Placar id="rp" nome="Reginaldo" />
+      {/* <Placar id="ab" nome="Abner" /> */}
     </div>
   );
 }
--- src/Componentes/placar.js
+++ src/Componentes/placar.js
@@ -1,19 +1,21 @@
 import { Component } from "react";
 
 export default class Placar extends Component {
-    constructor () {
+    constructor(props) {
         super();
+        this.props = props;
     }
 
     render() {
         return (
             <div className="cont">
+                <div className="name">{this.props.nome || ""}</div>
                 <div className="box">
-                    <p id="pont" className="pont">0</p>
+                    <p id={this.props.id} className="pont">0</p>
                 </div>
                 <div className="buttons">
-                    <button className="btnPont" type="button" onClick={addPont}>+</button>
-                    <button className="btnPont" type="button" onClick={removePont}>-</button>
+                    <button className="btnPont" type="button" onClick={ () => { addPont(this.props.id) }}>+</button>
+                    <button className="btnPont" type="button" onClick={ () => { removePont(this.props.id) }}>-</button>
                 </div>
             </div>
         )
@@ -21,16 +23,16 @@ export default class Placar extends Component {
 }
 
 
-const addPont = () => {
-    const pont = document.getElementById('pont');
+function addPont (id) {
+    const pont = document.getElementById(id);
     if (pont !== null) {
-    pont.innerText = parseInt(pont.textContent)+1;
+        pont.innerText = parseInt(pont.textContent) + 1;
     }
 }
 
-const removePont = () => {
-    const pont = document.getElementById('pont');
+function removePont (id) {
+    const pont = document.getElementById(id);
     if (pont !== null) {
-        pont.innerText = parseInt(pont.textContent)-1;
+        pont.innerText = parseInt(pont.textContent) - 1;
     }
 }
